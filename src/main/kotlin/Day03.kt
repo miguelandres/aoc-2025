24094e655cdccb34b9b083b0a2a90b7576600ca6
Miguel Barreto <miguelandres@users.noreply.github.com>
2025-12-09 22:32:14 -0500

    add day 3

---
+++ src/main/kotlin/Day03.kt
@@ -0,0 +1,39 @@
+import utils.readAocInput
+
+fun main() {
+  val powerBanks =
+    readAocInput(3, 1)
+      .map { it.map { it.digitToInt() } }
+
+  val part1 =
+    powerBanks.sumOf {powerBank ->
+      var maxDigit = 0
+      var maxDigitPosition = 0
+      for (i in (0 until powerBank.size - 1)) {
+        if (powerBank[i] > maxDigit) {
+          maxDigit = powerBank[i]
+          maxDigitPosition = i
+        }
+      }
+      val secondDigit = powerBank.subList(maxDigitPosition + 1, powerBank.size).max()
+      maxDigit * 10 + secondDigit
+    }
+  println(part1)
+
+  val part2 =
+    powerBanks.sumOf {powerBank ->
+      var runningPowerBank = powerBank
+      var runningValue = 0L
+      for (i in 1..12) {
+        runningValue *= 10
+        val candidates = runningPowerBank.subList(0, runningPowerBank.size - 12 + i)
+        val maxCandidate = candidates.max()
+        runningValue += maxCandidate
+        val position = candidates.indexOf(maxCandidate)
+        runningPowerBank = runningPowerBank.subList(position + 1, runningPowerBank.size)
+      }
+      runningValue
+    }
+
+  println(part2)
+}

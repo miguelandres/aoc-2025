c9ab2e2c0f0ffb493d8511205a24c3b113558290
Miguel Barreto <miguelandres@users.noreply.github.com>
2025-12-10 22:42:32 -0500

    Make the template start with part 0

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -2,5 +2,5 @@ import utils.readAocInput
 
 fun main() {
   @Suppress("UNUSED_VARIABLE")
-  val lines = readAocInput(1).map { s -> s.toIntOrNull() }
+  val lines = readAocInput(1, 0).map { s -> s.toIntOrNull() }
 }

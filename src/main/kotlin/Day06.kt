8ec1911e863d63bbc8b0628ff90c1909ea887d8c
Miguel Barreto <miguelandres@users.noreply.github.com>
2025-12-12 17:48:03 -0500

    day6part2

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -39,6 +39,7 @@ fun main() {
       Map2D(it)
     }
 
+  val operationsLine = lines.last()
   val operations =
     lines.last().split(whitespaceRegex).map {
       when (it) {
@@ -53,4 +54,31 @@ fun main() {
       numbers.range.rangeY.map { j -> numbers[Position(i, j)] }.reduce { a, b -> operation.apply(a, b) }
     }.sum()
   println(part1)
+
+  val startIndices = mutableListOf<Int>()
+  for (i in 0 until operationsLine.length) {
+    if (operationsLine[i] != ' ') startIndices.add(i)
+  }
+  val endIndices = startIndices.subList(1, startIndices.size).map { it - 2} + listOf(lines.maxOf{ it.length} - 1)
+  val numberRanges = startIndices.zip(endIndices).map { it.first..it.second }
+
+  val part2 =
+    (0 until operations.size).sumOf { opNumber ->
+      val numberRange = numberRanges[opNumber]
+      val operation = operations[opNumber]
+      numberRange.map { column ->
+        numbersStr.fold(0L) { acc, row ->
+          if (column >= row.length) {
+            // No op, empty space
+            acc
+          } else if (row[column].isDigit()) {
+            acc * 10 + row[column].digitToInt()
+          } else {
+            acc
+          }
+        }
+      }.reduce { a, b -> operation.apply(a, b) }
+    }
+
+  println(part2)
 }
